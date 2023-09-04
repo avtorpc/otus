e0ee7bd3864e4d6cc9e8f764bb868a7509750c85
dennis <avtorpc@gmail.com>
2023-09-04 16:16:20 +0300

    ---Сборка метод 2---Почему сборка с модулем shadow создает два jar?---Один fat gradleHelloOTUS а второй hw1-gradle похоже что тонкий?---

---
+++ hw1-gradle/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import ru.otus.example.OtusHello;
+
+public class Main {
+    public static void main(String[] args) {
+
+        OtusHello hello = new OtusHello( null, "OTUS");
+
+        hello.getHello();
+    }
+}
